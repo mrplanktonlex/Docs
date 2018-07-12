930db1ebf1909aa257ef76cc6304291c213714ac
mrplanktonlex <34418849+mrplanktonlex@users.noreply.github.com>
2018-07-12 13:14:15 +0530

    Add files via upload

---
+++ jonas_advanced_css/0jonasReposNContent/Comp__btn-ripple-inside/btn-ripple-inside.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { BtnRippleInsideComponent } from './btn-ripple-inside.component';
+
+// describe('BtnRippleInsideComponent', () => {
+//   let component: BtnRippleInsideComponent;
+//   let fixture: ComponentFixture<BtnRippleInsideComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ BtnRippleInsideComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(BtnRippleInsideComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ jonas_advanced_css/0jonasReposNContent/Comp__btn-ripple-inside/btn-ripple-inside.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-ripple-inside',
+  templateUrl: './btn-ripple-inside.component.html',
+  styleUrls: ['./btn-ripple-inside.component.css']
+})
+export class BtnRippleInsideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
