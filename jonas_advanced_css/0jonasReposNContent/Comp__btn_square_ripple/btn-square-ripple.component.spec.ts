7bc39ecdce9d03272cd627992586e8dbabfb2abb
mrplanktonlex <34418849+mrplanktonlex@users.noreply.github.com>
2018-07-12 13:06:15 +0530

    Add files via upload

---
+++ jonas_advanced_css/0jonasReposNContent/Comp__btn_square_ripple/btn-square-ripple.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { BtnSquareRippleComponent } from './btn-square-ripple.component';
+
+// describe('BtnSquareRippleComponent', () => {
+//   let component: BtnSquareRippleComponent;
+//   let fixture: ComponentFixture<BtnSquareRippleComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ BtnSquareRippleComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(BtnSquareRippleComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ jonas_advanced_css/0jonasReposNContent/Comp__btn_square_ripple/btn-square-ripple.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-square-ripple',
+  templateUrl: './btn-square-ripple.component.html',
+  styleUrls: ['./btn-square-ripple.component.css']
+})
+export class BtnSquareRippleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
