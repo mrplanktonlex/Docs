b5ef9597235e55a88d24cce829993f409379092a
mrplanktonlex <34418849+mrplanktonlex@users.noreply.github.com>
2018-07-12 12:58:41 +0530

    Add files via upload

---
+++ jonas_advanced_css/0jonasReposNContent/Comp__btn-rounded-ripple/btn-rounded-ripple.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { BtnRoundedRippleComponent } from './btn-rounded-ripple.component';
+
+// describe('BtnRoundedRippleComponent', () => {
+//   let component: BtnRoundedRippleComponent;
+//   let fixture: ComponentFixture<BtnRoundedRippleComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ BtnRoundedRippleComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(BtnRoundedRippleComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ jonas_advanced_css/0jonasReposNContent/Comp__btn-rounded-ripple/btn-rounded-ripple.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-rounded-ripple',
+  templateUrl: './btn-rounded-ripple.component.html',
+  styleUrls: ['./btn-rounded-ripple.component.css']
+})
+export class BtnRoundedRippleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
