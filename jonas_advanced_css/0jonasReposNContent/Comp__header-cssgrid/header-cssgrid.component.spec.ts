4ba9458c27d6ff0c1b0653f06114eccfae28519c
mrplanktonlex <34418849+mrplanktonlex@users.noreply.github.com>
2018-07-13 15:42:11 +0530

    Add files via upload

---
+++ jonas_advanced_css/0jonasReposNContent/Comp__header-cssgrid/header-cssgrid.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { HeaderCssgridComponent } from './header-cssgrid.component';
+
+// describe('HeaderCssgridComponent', () => {
+//   let component: HeaderCssgridComponent;
+//   let fixture: ComponentFixture<HeaderCssgridComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ HeaderCssgridComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(HeaderCssgridComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ jonas_advanced_css/0jonasReposNContent/Comp__header-cssgrid/header-cssgrid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-cssgrid',
+  templateUrl: './header-cssgrid.component.html',
+  styleUrls: ['./header-cssgrid.component.css']
+})
+export class HeaderCssgridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
