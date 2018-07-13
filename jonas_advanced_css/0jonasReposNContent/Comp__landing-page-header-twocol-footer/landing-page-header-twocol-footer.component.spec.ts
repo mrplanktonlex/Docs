d90cbe2f213af422ded785e8714f41fa50e6d8ba
mrplanktonlex <34418849+mrplanktonlex@users.noreply.github.com>
2018-07-13 16:36:24 +0530

    Add files via upload

---
+++ jonas_advanced_css/0jonasReposNContent/Comp__landing-page-header-twocol-footer/landing-page-header-twocol-footer.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { LandingPageHeaderTwocolFooterComponent } from './landing-page-header-twocol-footer.component';
+
+// describe('LandingPageHeaderTwocolFooterComponent', () => {
+//   let component: LandingPageHeaderTwocolFooterComponent;
+//   let fixture: ComponentFixture<LandingPageHeaderTwocolFooterComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ LandingPageHeaderTwocolFooterComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(LandingPageHeaderTwocolFooterComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ jonas_advanced_css/0jonasReposNContent/Comp__landing-page-header-twocol-footer/landing-page-header-twocol-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-header-twocol-footer',
+  templateUrl: './landing-page-header-twocol-footer.component.html',
+  styleUrls: ['./landing-page-header-twocol-footer.component.css']
+})
+export class LandingPageHeaderTwocolFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
