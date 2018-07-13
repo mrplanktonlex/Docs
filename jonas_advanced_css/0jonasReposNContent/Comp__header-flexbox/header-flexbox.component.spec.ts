c6aa93c57d26235d58d578cf186e8cec02643795
mrplanktonlex <34418849+mrplanktonlex@users.noreply.github.com>
2018-07-13 15:25:03 +0530

    Add files via upload

---
+++ jonas_advanced_css/0jonasReposNContent/Comp__header-flexbox/header-flexbox.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { HeaderFlexboxComponent } from './header-flexbox.component';
+
+// describe('HeaderFlexboxComponent', () => {
+//   let component: HeaderFlexboxComponent;
+//   let fixture: ComponentFixture<HeaderFlexboxComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ HeaderFlexboxComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(HeaderFlexboxComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ jonas_advanced_css/0jonasReposNContent/Comp__header-flexbox/header-flexbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-flexbox',
+  templateUrl: './header-flexbox.component.html',
+  styleUrls: ['./header-flexbox.component.css']
+})
+export class HeaderFlexboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
